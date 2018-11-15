5c4021361e0bb07fd8bddc6b006e371cd361286d
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-11-15 12:35:09 -0500

    bootstrap added

--- Promises/fetch.js
+++ Promises/fetch.js
@@ -2,6 +2,7 @@ function createUser() {
   // This function will grab all of the inputs for the user and send it to the JSON placeholder api via fetch.
   //
   // Send the name, username, email, and phone at a minimum
+  // https://jsonplaceholder.typicode.com/users as POST
   //
   // After the completion of the fetch, output a success/failure message to the user
 }
